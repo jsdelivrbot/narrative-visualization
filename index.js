3e01004e135d00eb964d963e0a8c1a009e7449e2
Madhvesh Navkal <madhvesh@gmail.com>
2018-07-08 12:44:49 -0700

    redirecting to HTML instead of ejs

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const path = require('path')
+var express = require("express");
+var bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + 'views'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views/pages/narration.html");
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+//
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+//
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   //.set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/test'))
+//   //.get('/', (req, res) => res.send('Hello world!'))
+//     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
